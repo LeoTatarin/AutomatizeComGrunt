6056e8a95fb09c388431992a5a877abdf32984cc
LeoTatarin <tatarinleo@gmail.com>
2025-06-23 22:15:25 -0300

    Criando tarefas

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,15 @@
-MediaSourceHandle.exports = function(grunt){
+module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
     })
+
+    grunt.registerTask('olaGrunt', function(){
+        const done = this.async();
+        setTimeout(function() {
+            console.log('Olá Grunt');
+            done();
+        }, 3000);
+    })
+
+    grunt.registerTask('default', ['olaGrunt']);
 }
